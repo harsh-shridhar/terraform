d7db1fcfe4adbee582e3e4e0ecf23029b1b6d23d
Harshendu Sridhar <Harshendu.Sridhar@intelematics.com>
2018-02-26 10:50:40 +1100

    changed the ami ID and region

--- example/main.tf
+++ example/main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  region     = "ap-northeast-1"
+  region     = "us-east-1"
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
 }
 
 resource "aws_instance" "example-ec2" {
-  ami           = "ami-2d39803a"
+  ami           = "ami-97785bed"
   instance_type = "t2.micro"
 
   tags {
